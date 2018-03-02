9db5f4b48574c8c375df7dc1787b22d9b0f6e597
Ryan <rigarium@gmail.com>
2018-03-02 15:57:20 -0500

    Slightly bigger markers, markers are now numbered
    No need to rearrange surveys now. Click through them top to bottom, add
    the markers to this map, right click to get rid of them and order
    persists

--- Scripts/CanvasMarker.gd
+++ Scripts/CanvasMarker.gd
@@ -1,10 +1,25 @@
 extends TextureRect
 
 var entered = false
+var id = 0
+var map = null
+
+func _ready():
+	map = get_parent().get_parent().get_node("Map")
 
 func _input(ev):
 	if ev.is_action_pressed("mouse_right") and entered == true:
+		map._remove_marker_and_renumber(id)
 		queue_free()
+
+func _queue_free():
+	queue_free()
+	
+func _set_id(value):
+	id = value
+	$ID.bbcode_text = "[center]"
+	$ID.bbcode_text += str(id)
+	$ID.bbcode_text += "[/center]"
 
 func _on_CanvasMarker_mouse_entered():
 	entered = true
--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -3,6 +3,7 @@ extends TextureRect
 var marker = preload("res://CanvasMarker.tscn")
 var entered = false
 var colorPicker = null
+var markerCount = 0
 
 func _ready():
 	colorPicker = get_parent().get_node("MouseColors").get_node("ColorPickerButton")
@@ -16,6 +17,25 @@ func _on_Map_mouse_exited():
 func _input(ev):
 	if ev.is_action_pressed("mouse_left") and entered == true:
 		var m = marker.instance()
-		m.rect_position = get_viewport().get_mouse_position() - Vector2(433, 10)
-		m.modulate = colorPicker.get_pick_color()
+		_add_marker(m)
 		add_child(m)
+		
+func _add_marker(m):
+	m.rect_position = get_viewport().get_mouse_position() - Vector2(439, 16)
+	m.modulate = colorPicker.get_pick_color()
+	markerCount += 1
+	m._set_id(markerCount)
+
+func _remove_all_markers():
+	markerCount = 0
+	for n in get_children():
+		n.queue_free()
+	
+	
+func _remove_marker_and_renumber(id):
+	markerCount -= 1
+	for n in get_children():
+		if n.id != id:
+			if n.id > id:
+				n.id -= 1
+				n._set_id(n.id)
